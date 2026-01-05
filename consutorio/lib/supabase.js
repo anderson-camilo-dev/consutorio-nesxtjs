11b4cc572533459c272fa7b015c2a0ff90dcea0c
anderson-camilo-dev <andersoncamilodev@gmail.com>
2026-01-05 17:01:21 -0300

    começamos a imprementar o subapase e falta integrar ao frontend

---
+++ consutorio/lib/supabase.js
@@ -0,0 +1,9 @@
+// lib/supabase.js
+
+import { createClient } from '@supabase/supabase-js';
+
+// Substitua pelos seus valores do Supabase
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
